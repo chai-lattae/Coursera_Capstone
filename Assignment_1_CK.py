922d310fb451943732e373ed2946044bea90fe2b
chai-lattae <61914876+chai-lattae@users.noreply.github.com>
2020-06-25 01:35:05 +0530

    Add files via upload

---
+++ Assignment_1_CK.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This notebook will mainly be used for the Coursera IBM Applied Data Science Capstone Project
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
